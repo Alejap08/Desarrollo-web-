360057f2bf8d4a1afa09ae897f1bff04bcc3045f
Alejap08 <99819712+Alejap08@users.noreply.github.com>
2022-03-30 11:46:23 -0500

    Add files via upload

---
+++ desarrollo web/app.js
@@ -0,0 +1,20 @@
+//import {pi} from './math.js';
+//import {IniciarJ, Saludar} from './saludar.js';
+
+//console.log(Saludar('Juan'));
+//console.log(pi);
+
+//console.log (IniciarJ('EN MINUTOS VA'))
+//const titulo = document.getElementById('Titulo');
+
+//titulo.innerHTML = 'BIENVENIDO';
+
+//const titulo = document.getElementById('Titulo1');
+
+//titulo.innerHTML = 'PIEDRA';
+
+//const titulo = document.getElementById('principal');
+
+//titulo.innerHTML = 'BIENVENIDOS AL JUEGO';//
+
+import {generateComputersChoice,getResult} from "./prueba.js";
---
+++ desarrollo web/saludar.js
@@ -0,0 +1,10 @@
+
+export const Saludar = (nombre) => {
+    return `Buenos días, ${nombre}`;
+}
+
+export const pedirAyuda =() => 'Ayuda';
+
+export const IniciarJ =(incio) => {
+    return `INICIAR EL JUEGO, ${incio}`;
+}
